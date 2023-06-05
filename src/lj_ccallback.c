eab58f52feacf1902c04af4381b98dcc4cca3047
Nicolas Noble <nicolasnoble@users.noreply.github.com>
2023-06-05 06:32:07 -0700

    Ensure proper JIT execution context on C callbacks

--- src/lj_ccallback.c
+++ src/lj_ccallback.c
@@ -681,6 +681,11 @@ lua_State * LJ_FASTCALL lj_ccallback_enter(CTState *cts, void *cf)
   lua_State *L = cts->L;
   global_State *g = cts->g;
   lj_assertG(L != NULL, "uninitialized cts->L in callback");
+#if LJ_HASJIT
+  jit_State *J = G2J(g);
+  J->L = L;
+#endif
+  setgcref(g->cur_L, obj2gco(L));
   if (tvref(g->jit_base)) {
     setstrV(L, L->top++, lj_err_str(L, LJ_ERR_FFI_BADCBACK));
     if (g->panic) g->panic(L);
